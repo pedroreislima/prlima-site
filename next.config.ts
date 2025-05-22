1dd88ae6276dbc6e32c8075e45351158365c894c
Pedro Reis Lima <66177749+gaffe123@users.noreply.github.com>
2025-05-22 23:04:19 +0000

    fixed the path prefix to next.config to see if it works

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  basePath: "/PRLIMA-SITE",
+  //basePath: "/PRLIMA-SITE",
   output: "export",
   reactStrictMode: true,
 };
