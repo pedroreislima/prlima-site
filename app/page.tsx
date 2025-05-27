4c124ed84ac186e992ec6730197f85dc88934263
pedroreislima <gaffetwo@gmail.com>
2025-05-27 12:58:21 -0300

    added more stylization on home page and more info.

--- app/page.tsx
+++ app/page.tsx
@@ -1,28 +1,67 @@
 
 export default function Home() {
   return (
-    <div className="bg-black text-white pb-10 ubuntuSans.className">
-      <h1 className="text-3xl font-bold max-w-[80ch] mx-auto p-5">
-        Pedro Reis Lima
-      </h1>
-      <div className="grid lg:grid-cols-[30%_70%] grid-cols-1 gap-5 max-w-[80ch] mx-auto p-5">
-        <div className="lg:max-w-50 max-w-80 mx-auto"><img src = "images/Foto pessoal.jpg" className="h-auto"/></div>
-
-        <div className="lg:row-start-2 flex item-center justify-center lg:gap-0 gap-10"> 
-          <a href = "https://www.linkedin.com/in/pedroreisrl/"><svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 16 16" className = "lg:w=8 lg:h-8 w-16 h-16">
-            <path d="M0 1.146C0 .513.526 0 1.175 0h13.65C15.474 0 16 .513 16 1.146v13.708c0 .633-.526 1.146-1.175 1.146H1.175C.526 16 0 15.487 0 14.854zm4.943 12.248V6.169H2.542v7.225zm-1.2-8.212c.837 0 1.358-.554 1.358-1.248-.015-.709-.52-1.248-1.342-1.248S2.4 3.226 2.4 3.934c0 .694.521 1.248 1.327 1.248zm4.908 8.212V9.359c0-.216.016-.432.08-.586.173-.431.568-.878 1.232-.878.869 0 1.216.662 1.216 1.634v3.865h2.401V9.25c0-2.22-1.184-3.252-2.764-3.252-1.274 0-1.845.7-2.165 1.193v.025h-.016l.016-.025V6.169h-2.4c.03.678 0 7.225 0 7.225z"/>
-          </svg></a>
-          <a href = "https://github.com/pedroreislima"><svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 16 16" className = "lg:w=8 lg:h-8 w-16 h-16">
-            <path d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27s1.36.09 2 .27c1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.01 8.01 0 0 0 16 8c0-4.42-3.58-8-8-8"/>
-          </svg></a>
+    <div>
+      {
+        // about me landing section
+      }
+      <div className="bg-primary text-secondary pb-10">
+        <div className = "max-w-[120ch] mx-auto">
+          <h1 className="text-3xl font-bold mx-auto pt-5 pb-5">
+            Pedro Reis Lima
+          </h1>
         </div>
+        
+        <div className="grid lg:grid-cols-[30%_70%] grid-cols-1 gap-5 max-w-[120ch] mx-auto p-5">
+          <div className="lg:max-w-50 max-w-80 mx-auto"><img src = "images/Foto pessoal.jpg" className="h-auto"/></div>
 
-        <div className="lg:row-span-2 w-auto flex items-center justify-center p-5">
-          <p className="text-justify p-5">
-            Meu nome é Pedro e sou Economista e Cientista de Dados. Mais especificamente, sou formado em economia e também mestre pela Universidade Federal do Ceará.
-          </p>
+          <div className="lg:row-start-2 flex item-center justify-center lg:gap-0 gap-10"> 
+            <a href = "https://www.linkedin.com/in/pedroreisrl/"><svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 16 16" className = "lg:w=8 lg:h-8 w-16 h-16 hover:fill-accent">
+              <path d="M0 1.146C0 .513.526 0 1.175 0h13.65C15.474 0 16 .513 16 1.146v13.708c0 .633-.526 1.146-1.175 1.146H1.175C.526 16 0 15.487 0 14.854zm4.943 12.248V6.169H2.542v7.225zm-1.2-8.212c.837 0 1.358-.554 1.358-1.248-.015-.709-.52-1.248-1.342-1.248S2.4 3.226 2.4 3.934c0 .694.521 1.248 1.327 1.248zm4.908 8.212V9.359c0-.216.016-.432.08-.586.173-.431.568-.878 1.232-.878.869 0 1.216.662 1.216 1.634v3.865h2.401V9.25c0-2.22-1.184-3.252-2.764-3.252-1.274 0-1.845.7-2.165 1.193v.025h-.016l.016-.025V6.169h-2.4c.03.678 0 7.225 0 7.225z"/>
+            </svg></a>
+            <a href = "https://github.com/pedroreislima"><svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 16 16" className = "lg:w=8 lg:h-8 w-16 h-16 hover:fill-accent">
+              <path d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27s1.36.09 2 .27c1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.01 8.01 0 0 0 16 8c0-4.42-3.58-8-8-8"/>
+            </svg></a>
+          </div>
+
+          <div className="lg:row-span-2 w-auto flex items-center justify-center p-5">
+            <p className="text-justify p-5">
+              Meu nome é Pedro e sou Economista e Cientista de Dados. Mais especificamente, sou formado em economia e também mestre pela Universidade Federal do Ceará.
+            </p>
+          </div>
         </div>
       </div>
+
+      <hr className = "p-5"/>
+
+      {
+        // education and work section
+      }
+      <div className = "lg:max-w-[120ch] lg:p-0 pl-4 pr-4 mx-auto">
+        <div className="pb-5 pt-5">
+          <h2 className = "font-bold text-3xl pb-1 border-b-2 border-primary">Formação</h2>
+        </div>
+
+        <div className="pb-5 ">
+          <h3 className = "text-xl">
+            <strong>Graduação | Ciências Econômicas</strong>, Universidade Federal do Ceará - Fortaleza, Brasil.
+          </h3>
+          <p className="text-primary/60"><strong>TCC:</strong> Reincidência Criminal: O caso brasileiro e revisão sistemática da literatura de avaliação de programas.</p>
+          <p className="text-primary/60">2017 - 2022</p>
+        </div>
+
+        <div className="pb-5 ">
+          <h3 className = "text-xl">
+            <strong>Mestrado | Economia</strong>, CAEN Universidade Federal do Ceará - Fortaleza, Brasil.
+          </h3>
+          <p className="text-primary/60"><strong>Dissertação:</strong> Analyzing criminal facility location of illegal airstrips in the Amazon.</p>
+          <p className="text-primary/60">2023 - 2025</p>
+        </div>
+
+      </div>
+      {
+        // projects
+      }
     </div>
   );
 }
--- components/Header.tsx
+++ components/Header.tsx
@@ -1,22 +1,22 @@
 export const Header = () => {
     return(
-        <header className="flex bg-black text-white px-2 py-4">
-            <div className="flex items-center w-full mx-auto justify-between pl-5 pr-5">
-                <div className = 'text-bold'>
+        <header className="flex bg-primary text-secondary  px-2 py-4 border-b border-secondary">
+            <div className="flex items-center w-full max-w-10xl mx-auto justify-between max-w-[120ch]">
+                <div className = 'text-bold hover:text-accent'>
                     <a href = '/prlima-site/'>Pedro Reis Lima</a>
                 </div>
                 <nav>
                     <ul className = "flex item-center gap-4">
-                        <li>
+                        <li className = "hover:text-accent">
                             <a href = '/prlima-site/academic'>academic</a>
                         </li>
-                        <li>
+                        <li className = "hover:text-accent">
                             <a href = '/prlima-site/projects'>projects</a>
                         </li>
                         <li>
                             <a download href = "CV/CV-prof Pedro Reis Lima.pdf" className = "flex gap-1 group">
-                                <h1 className = "group-has-hover:text-yellow-500">cv</h1> 
-                                <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 16 16" className="my-auto mx-auto w-4 h-4 group-has-hover:text-yellow-500">
+                                <h1 className = "group-has-hover:text-accent">cv</h1> 
+                                <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 16 16" className="my-auto mx-auto w-4 h-4 group-has-hover:text-accent">
                                     <path d="M8.5 6.5a.5.5 0 0 0-1 0v3.793L6.354 9.146a.5.5 0 1 0-.708.708l2 2a.5.5 0 0 0 .708 0l2-2a.5.5 0 0 0-.708-.708L8.5 10.293z"/>
                                     <path d="M14 14V4.5L9.5 0H4a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2M9.5 3A1.5 1.5 0 0 0 11 4.5h2V14a1 1 0 0 1-1 1H4a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1h5.5z"/>
                                 </svg>
